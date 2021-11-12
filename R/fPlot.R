d2ede3edcb56ae6a9dde7d626972ab197f4f27c0
mischko <mhem@mailbox.org>
2021-11-12 17:34:01 +0100

    update fplot

--- R/fPlot.R
+++ R/fPlot.R
@@ -5,12 +5,13 @@
 #' @param width width of output plot (default: 16)
 #' @param height height of output plot (default: length of genes divided by two)
 #' @return save feature plot to folder `featureplot`
-#' @examples FPlot(sc_merge, par = "main", width = 5, height = 5)
+#' @examples \dontrun{fPlot(sc_merge, par = "main", width = 5, height = 5)}
 #' @export
 
-
-FPlot <- function(object, par, width = 16, height = ceiling(length(genes)/2)) {
-    markers <- readxl::read_excel("./markers.xlsx") |>
+fPlot <- function(object, par, width = 16, height = ceiling(length(genes)/2)) {
+    dir.create(here::here("featureplot"), showWarnings = FALSE)
+    stopifnot(file.exists("markers.csv"))
+    markers <- readr::read_csv(here::here("markers.csv")) |>
     as.list(markers) |>
     lapply(function(x) x[!is.na(x)])
 object_parse <- deparse(substitute(object))
@@ -19,7 +20,5 @@ fp <- Seurat::FeaturePlot(object = object, features = unique(genes), cols = viri
     theme(axis.text = element_blank(),
           axis.ticks = element_blank(),
           panel.border = element_rect(color = "black", size = 1, fill = NA))
-ggsave(file.path(project_path, "featureplot", glue::glue("{object_parse}_{par}.png")), width = width, height = height, limitsize = FALSE)
+ggsave(here::here(project_path, "featureplot", glue::glue("{object_parse}_{par}.png")), width = width, height = height, limitsize = FALSE)
 }
-
-
